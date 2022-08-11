9438fc6a7d476d94829306c8de7cb78c401c9841
654a <99504903+654a@users.noreply.github.com>
2022-08-11 13:58:40 +0300

    Update 0-print_list.c

--- 0-print_list.c
+++ 0-print_list.c
@@ -1,49 +1,25 @@
 #include "lists.h"
 
 /**
-  * print_list - Prints all the elements of a linked list
-  * @h: Pointer to the head of the list
-  * Return: The number of nodes
-  */
+*print_list - prints all elements of list_t list
+*@h: linked list
+*Return: number of nodes in linked list
+*/
 
 size_t print_list(const list_t *h)
-
 {
+size_t count = 0;
 
-	int count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
 
+count++;
+h = h->next;
+}
 
-
-	while (h != NULL)
-
-	{
-
-		if (h->str == NULL)
-
-		{
-
-			printf("[0] (nil)\n");
-
-			count++;
-
-			h = h->next;
-
-		}
-
-		else
-
-		{
-
-			printf("[%u] %s\n", h->len, h->str);
-
-			count++;
-
-			h = h->next;
-
-		}
-
-	}
-
-	return (count);
-
+return (count);
 }
