ba690889730c4ae5feda7e0a268473e13907c3fd
654a <99504903+654a@users.noreply.github.com>
2022-08-11 14:43:33 +0300

    Update 4-free_list.c

--- 4-free_list.c
+++ 4-free_list.c
@@ -1,17 +1,19 @@
 #include "lists.h"
 
 /**
-*free_list - free linked list
-*@head: linked list
-*/
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
 
 void free_list(list_t *head)
 {
-while (head != NULL)
-{
-free(head->str);
-free(head);
-head = head->next;
-}
-}
+	list_t *current_node;
 
+	while ((current_node = head) != NULL)
+	{
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
