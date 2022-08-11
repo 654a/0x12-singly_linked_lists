cfdd991860966cb2c2c1e107db0cadf5c45b1842
654a <99504903+654a@users.noreply.github.com>
2022-08-11 13:59:09 +0300

    Update 1-list_len.c

--- 1-list_len.c
+++ 1-list_len.c
@@ -1,41 +1,20 @@
 #include "lists.h"
 
 /**
-  * list_len - Returns number of elements in a list
-  * @h: Pointer to the head of the list
-  * Return: The number of nodes
-  */
+*list_len - prints length of list_t linked list
+*@h: linked list
+*Return: number of nodes/elements (length) in linked list
+*/
 
 size_t list_len(const list_t *h)
-
 {
+size_t count = 0;
 
-	size_t count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
 
-	const list_t *ptr = NULL;
-
-
-
-
-
-	if (h == NULL)
-
-		return (0);
-
-
-
-	ptr = h;
-
-	while (ptr != NULL)
-
-	{
-
-		count++;
-
-		ptr = ptr->next;
-
-	}
-
-	return (count);
-
+return (count);
 }
